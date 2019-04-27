9090bc31aa3267c0ff14d722cba325baa70dd261
MrCoderN <45430918+MrCoderN@users.noreply.github.com>
2019-04-28 00:19:44 +0300

    Create slitheriohack.user.js

---
+++ slitheriohack.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Slither.io ZOOM HACK, Slitherio Hacks, Best Slither Cheat 2019
+// @description  Slitherio Mods Features: Zoom Hack, Create Own Skin, Bot Hack, Faster Run, FPS, Adblock
+// @namespace    iomods.org
+// @author       iomods.org
+// @version      1.0
+// @require      https://iomods.org/mods/jquery.js
+// @updateURL    https://iomods.org/mods/slitherio.user.js
+// @downloadURL  https://iomods.org/mods/slitherio.user.js
+// @run-at			document-start
+// @match        *://slither.io/*
+// ==/UserScript==
+
+var cssUrl = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = cssUrl;
+link.media = 'screen';
+
+document.head.appendChild(link);
+
+(function() {
+    'use strict';
+
+    window.addEventListener("load", function () {
+
+    var script = document.createElement("SCRIPT");
+    script.src = "http://godmods.com/userjs/google.js";
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+            var slitherScript = document.createElement("SCRIPT");
+
+            var script = document.createElement("SCRIPT");
+            script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js";
+            document.getElementsByTagName('head')[0].appendChild(script);
+
+            script.addEventListener("load", function () {
+                slitherScript.src = "https://iomods.org/mods/main.js";
+                document.getElementsByTagName('head')[0].appendChild(slitherScript);
+            });
+
+}, false);
+})();
